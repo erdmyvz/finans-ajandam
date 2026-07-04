c58065822e32002723305ef7d057e632e5d71d74
Erdem Yavuz <erdem.yvz@hotmail.com>
2026-07-04 11:27:01 +0300

    Pro_V3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finans-v1';
+const CACHE_NAME = 'finans-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,18 +7,33 @@ const urlsToCache = [
   './manifest.json'
 ];
 
+// Yeni versiyonu kur
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Yeni versiyonu hemen devreye al
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Eski versiyon (v1) önbelleğini temizle
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Network First Stratejisi (İnternet varsa yeniyi al, yoksa cache'ten al)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
